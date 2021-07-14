002d8bcd2cc13a88a8e604d6aa34876186adda6e
Rafael etc <rafaeletc@users.noreply.github.com>
2021-07-13 22:34:35 -0300

    tapa na groselha

--- main.c
+++ main.c
@@ -6,13 +6,23 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 20:00:37 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/07/13 20:01:08 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/07/13 22:21:58 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
 #include "get_next_line.h"
 
-int	main(void)
+void	conv_lower(void)
+{
+	int	c;
+
+	c = getchar();
+	while (c != EOF)
+		putchar(tolower(c));
+}
+
+void	static_call(void)
 {
 	printf("Chamada estática c/ static_func()\n");
 	static_func();
@@ -20,11 +30,22 @@ int	main(void)
 	static_func();
 	static_func();
 	static_func();
+}
+
+void	dynamic_call(void)
+{
 	printf("\nChamada dinâmica c/ dynamic_func()\n");
 	dynamic_func();
 	dynamic_func();
 	dynamic_func();
 	dynamic_func();
 	dynamic_func();
+}
+
+int	main(void)
+{
+	static_call();
+	dynamic_call();
+	conv_lower();
 	return (0);
 }

f42fd6d25ecf68dad544325e4918d0d6d43f4817
Rafael etc <rafaeletc@users.noreply.github.com>
2021-07-17 16:41:38 -0300

    Estudos

---
+++ sndbx/test_getline.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_getline.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/13 20:00:37 by rde-lima          #+#    #+#             */
+/*   Updated: 2021/07/17 16:21:33 by rde-lima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define _GNU_SOURCE
+
+int	main(int argc, char *argv[])
+{
+	FILE	*stream;
+	char	*line;
+	int		line_count;
+	size_t	len;
+	ssize_t	nread;
+
+	line = NULL;
+	len = 0;
+	line_count = 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	stream = fopen(argv[1], "r");
+	if (stream == NULL)
+	{
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+	nread = getline(&line, &len, stream);
+	while (nread != -1)
+	{
+		++line_count;
+		printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s",
+			line_count, nread, len, line);
+		nread = getline(&line, &len, stream);
+	}
+	free(line);
+	fclose(stream);
+	exit(EXIT_SUCCESS);
+}
---
+++ sndbx/test_static.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_static.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/13 20:00:37 by rde-lima          #+#    #+#             */
+/*   Updated: 2021/07/17 16:34:55 by rde-lima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+/*
+** Declaração da variável estática só inicializa uma vez durante a execução,
+** independente da quantidade de vezes que for chamada. Aqui atribuo o valor 0
+** (se não tivesse atribuido, seria esse valor, automagicamente).
+** Parece que a norminette aceita declaração assim, não conferi a norma.
+*/
+void	static_func(void)
+{
+	static int	x = 0;
+
+	++x;
+	printf("%d\n", x);
+}
+
+/*
+** Já uma variável não estática é inicializada toda vez que a função
+** for chamada durante a execução.
+*/
+void	dynamic_func(void)
+{
+	int	x;
+
+	x = 0;
+	++x;
+	printf("%d\n", x);
+}
+
+void	static_call(void)
+{
+	printf("Chamada estática c/ static_func()\n");
+	static_func();
+	static_func();
+	static_func();
+	static_func();
+	static_func();
+}
+
+void	dynamic_call(void)
+{
+	printf("\nChamada dinâmica c/ dynamic_func()\n");
+	dynamic_func();
+	dynamic_func();
+	dynamic_func();
+	dynamic_func();
+	dynamic_func();
+}
+
+int	main(void)
+{
+	static_call();
+	dynamic_call();
+}

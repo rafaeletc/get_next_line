580c86e551cbcb249281772245f57ac4aee0c750
Rafael etc <rafaeletc@users.noreply.github.com>
2021-07-21 05:29:47 -0300

    novos testes

--- sndbx/test_get_next_line.c
+++ sndbx/test_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/18 13:01:10 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/07/19 12:33:12 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/07/21 05:21:20 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,48 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+void	test4(void);
+
 int	main(void)
+{
+	int		fd;
+	int		i;
+
+	fd = open("gnlTester/files/big_line_no_nl", O_RDONLY, 0);
+	i = 0;
+	while (i++ < 9)
+		printf("%s", get_next_line(fd));
+	/*fd = open("sndbx/file1", O_RDONLY, 0);
+	i = 0;
+	printf("file1\n");
+	while (++i <= 3)
+		get_next_line(fd);*/
+	return (0);
+}
+
+void	test4(void)
 {
 	int	fd;
 	int	i;
 
+	fd = open("sndbx/file1", O_RDONLY, 0);
+	i = 0;
+	printf("file1\n");
+	while (++i <= 3)
+		printf("%s", get_next_line(fd));
 	fd = open("sndbx/file2", O_RDONLY, 0);
 	i = 0;
-	while (++i <= 10)
-		printf("[%02d] - %s\n", i, get_next_line(fd));
-	return (0);
+	printf("\nfile2\n");
+	while (++i <= 9)
+		printf("%s", get_next_line(fd));
+	fd = open("sndbx/file3", O_RDONLY, 0);
+	i = 0;
+	printf("\nfile3\n");
+	while (++i <= 9)
+		printf("%s", get_next_line(fd));
+	fd = open("sndbx/file4", O_RDONLY, 0);
+	i = 0;
+	printf("\nfile4\n");
+	while (++i <= 9)
+		printf("%s", get_next_line(fd));
 }
---
+++ sndbx/test_read.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_read.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 20:31:19 by rde-lima          #+#    #+#             */
+/*   Updated: 2021/07/20 20:59:20 by rde-lima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <bsd/string.h>
+
+int	main(void)
+{
+	int		fd;
+	int		i;
+	char	*buf;
+
+	buf = malloc(sizeof(char) * 16);
+	fd = open("sndbx/lusiadas.txt", O_RDONLY, 0);
+	i = 0;
+	while (i < 99)
+	{
+		read(fd, buf, 16);
+	}
+}

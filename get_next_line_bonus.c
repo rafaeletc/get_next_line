356a5d8b37d020e252d8678b93ae29179e441ec4
Rafael de Lima Franco <rafaeletc@users.noreply.github.com>
2021-10-17 18:29:20 -0300

    reorganizado e limpeza de redundância ft_getline()

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,15 +6,15 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 14:02:04 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/10/17 17:41:46 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/10/17 18:26:58 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
 char	*ft_getline(int fd, char **buf, char **cache);
-char	*ft_writecache(char **cache);
 ssize_t	ft_readfile(int fd, char **buf, char **cache);
+char	*ft_writecache(char **cache);
 
 char	*get_next_line(int fd)
 {
@@ -36,11 +36,11 @@ char	*get_next_line(int fd)
 
 char	*ft_getline(int fd, char **buf, char **cache)
 {
-	ssize_t	file;
+	ssize_t	size;
 	char	*res;
 
-	file = ft_readfile(fd, buf, cache);
-	if (file <= 0 && !**cache)
+	size = ft_readfile(fd, buf, cache);
+	if (size <= 0 && !**cache)
 	{
 		free(*cache);
 		*cache = ft_strdup("");
@@ -48,31 +48,9 @@ char	*ft_getline(int fd, char **buf, char **cache)
 	}
 	if (ft_strchr(*cache, '\n'))
 		return (ft_writecache(cache));
-	if (!ft_strchr(*cache, '\n') && **cache)
-	{
-		res = ft_strdup(*cache);
-		free(*cache);
-		*cache = ft_strdup("");
-		return (res);
-	}
-	return (NULL);
-}
-
-char	*ft_writecache(char **cache)
-{
-	size_t	size;
-	char	*res;
-	char	*tmp;
-
-	size = 0;
-	while ((*cache)[size] != '\n' && (*cache)[size] != '\0')
-		++size;
-	if ((*cache)[size] == '\n')
-		++size;
-	tmp = *cache;
-	res = ft_substr(tmp, 0, size);
-	*cache = ft_strdup(&(*cache)[size]);
-	free(tmp);
+	res = ft_strdup(*cache);
+	free(*cache);
+	*cache = ft_strdup("");
 	return (res);
 }
 
@@ -92,5 +70,23 @@ ssize_t	ft_readfile(int fd, char **buf, char **cache)
 		*cache = ft_strjoin(tmp, *buf);
 		free(tmp);
 	}
+	return (res);
+}
+
+char	*ft_writecache(char **cache)
+{
+	ssize_t	size;
+	char	*res;
+	char	*tmp;
+
+	size = 0;
+	while ((*cache)[size] != '\n' && (*cache)[size] != '\0')
+		++size;
+	if ((*cache)[size] == '\n')
+		++size;
+	tmp = *cache;
+	res = ft_substr(tmp, 0, size);
+	*cache = ft_strdup(&(*cache)[size]);
+	free(tmp);
 	return (res);
 }
